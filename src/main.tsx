30170f5ec36a6b4151ed18a059951b09149a199c
T Mark <tmk1.tran@gmail.com>
2026-05-28 09:45:08 -0500

    add MUI, theme, font, AGENTS.md, and .nvmrc

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ApolloProvider } from "@apollo/client/react";
 import { apolloClient } from "./lib/apollo";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
 import "./index.css";
+import "@fontsource/nunito/400.css"; // regular 400
+// import "@fontsource/nunito/600.css"; // optional weight
+// import "@fontsource/nunito/700.css"; // optional weight
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
